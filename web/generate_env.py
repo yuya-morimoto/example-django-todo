3d26713c9d88fcae1a18f286fcfead9b81519d92
yuya-morimoto <my.hack.0312@gmail.com>
2023-09-16 19:39:06 +0900

    feat: setup

--- web/generate_env.py
+++ web/generate_env.py
@@ -1,5 +1,7 @@
 import os
+
 from django.core.management.utils import get_random_secret_key
+
 
 def generate_env():
     """
@@ -10,17 +12,27 @@ def generate_env():
     """
 
     # Check exist .evn file
-    is_exist_env_file: bool = os.path.exists('.env')
+    is_exist_env_file: bool = os.path.exists(".env")
     if is_exist_env_file:
         print('Already exists ".env"')
     else:
         secret_key: str = get_random_secret_key()
         writelines: list[str] = [
-            'ALLOWED_HOSTS=*\n'
-            f'SECRET_KEY={secret_key}'
+            "ALLOWED_HOSTS=*\n",
+            f"SECRET_KEY={secret_key}\n",
+            "\n",
+            "# Database\n",
+            "DATABASE=postgres\n",
+            "DB_ENGINE=django.db.backends.postgresql\n",
+            "DB_NAME=user\n",
+            "DB_USER=db\n",
+            "DB_PASSWORD=password\n",
+            "DB_HOST=db\n",
+            "DB_PORT=5432",
         ]
-        with open('.env', mode='w', newline='\n') as f:
+        with open(".env", mode="w", newline="\n") as f:
             f.writelines(writelines)
+
 
 if __name__ == "__main__":
     generate_env()
