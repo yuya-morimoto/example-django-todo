2065897d32e0942b4afb0ef8d0301b2499aa2848
yuya-morimoto <my.hack.0312@gmail.com>
2023-09-17 03:10:12 +0900

    feat: account app and auth

--- web/config/settings/common.py
+++ web/config/settings/common.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts",
+    "todos",
 ]
 
 MIDDLEWARE = [
@@ -67,7 +69,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR, "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- web/todos/models.py
+++ web/todos/models.py
@@ -1,1 +1,14 @@
-# Create your models here.
+import uuid
+
+from django.db import models
+
+
+class Todo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(
+        max_length=100,
+    )
+
+    class Meta:
+        verbose_name = "todo"
+        verbose_name_plural = "todos"
