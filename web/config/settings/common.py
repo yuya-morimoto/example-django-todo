d670209a43f0ecf7154b881bf222ea414f7ab5b3
yuya-morimoto <my.hack.0312@gmail.com>
2023-09-17 14:46:52 +0900

    feat: redis cache

--- web/config/settings/common.py
+++ web/config/settings/common.py
@@ -95,6 +95,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": env("REDIS_LOCATION"),
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
