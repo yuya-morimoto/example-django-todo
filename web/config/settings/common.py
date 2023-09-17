ef960f25d2d84cb58055e84cf5951e41040e971c
yuya-morimoto <my.hack.0312@gmail.com>
2023-09-17 16:27:38 +0900

    feat: auth url

--- web/config/settings/common.py
+++ web/config/settings/common.py
@@ -125,6 +125,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Authentication
+LOGIN_URL = "/accounts/signin"
+LOGIN_REDIRECT_URL = "/accounts"
+LOGOUT_REDIRECT_URL = "/accounts"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
