0c0e60bf2896609079024248756a24f8cd9b9219
yuya-morimoto <my.hack.0312@gmail.com>
2023-09-17 16:07:24 +0900

    feat: todo admin

--- web/todos/models.py
+++ web/todos/models.py
@@ -1,13 +1,31 @@
 import uuid
 
+from django.conf import settings
 from django.db import models
+
+
+class StatusChoices(models.TextChoices):
+    TODO = "01", "TODO"
+    DOING = "02", "DOING"
+    DONE = "03", "DONE"
 
 
 class Todo(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user_todos"
+    )
     title = models.CharField(
         max_length=100,
     )
+    description = models.TextField(max_length=400, null=True, blank=True)
+    status = models.CharField(
+        max_length=2,
+        choices=StatusChoices.choices,
+        default=StatusChoices.TODO,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = "todo"
